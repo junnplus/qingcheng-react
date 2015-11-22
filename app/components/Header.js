7080e1f27f3b5a6b491d8759af7e637da8408ffe
junnplus <junnplus@qq.com>
2015-11-22 21:53:10 +0800

    update header style

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,12 +3,15 @@ var HeaderNav = require('./HeaderNav');
 var UserAvatar = require('./UserAvatar');
 
 var Header = React.createClass({
+    style: function() {
+        var style = this.props.cafe.style;
+        if (!style || !style.cover) return {};
+        return {'backgroundImage': 'url(' + style.cover + ')'};
+    },
     render: function() {
-        var cafe = this.props.cafe ? this.props.cafe : {};
-        var user = this.props.user ? this.props.user : {};
         return (
             <div className="header">
-                <div className="header__cover cover">
+                <div className="header__cover cover" style={ this.props.cafe ? this.style() : {}}>
                     <div className="cover__text">
                         <div className="container">
                             <h2>{this.props.title}</h2><p dangerouslySetInnerHTML={{ __html: this.props.description }}></p>
@@ -17,7 +20,7 @@ var Header = React.createClass({
                 </div>
                 <div className="header__bottom">
                     <div className="container">
-                        <HeaderNav cafe={cafe} user={user} path={this.props.path} />
+                        <HeaderNav cafe={ this.props.cafe ? this.props.cafe : {} } user={ this.props.user ? this.props.user : {} } path={this.props.path} />
                     </div>
                 </div>
             </div>
