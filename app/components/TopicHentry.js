1a895e871462c173ccc148ce3cf63a8fd029dc35
junnplus <junnplus@qq.com>
2015-11-22 22:28:01 +0800

    topic and cafe style update

--- app/components/TopicHentry.js
+++ app/components/TopicHentry.js
@@ -4,23 +4,38 @@ var LikeButton = require('./LikeButton');
 var ShareButton = require('./ShareButton');
 var UserAvatar = require('./UserAvatar');
 var timeago = require('../filters').timeago;
+var urlize = require('../filters').urlize;
 
 var TopicHentry = React.createClass({
+	topicStyle: function() {
+		var cover = this.props.topic.info.cover;
+		if (!cover) return null;
+		return {'backgroundImage': 'url(' + cover + ')'};
+	},
+	cafeStyle: function() {
+		var style = this.props.topic.cafes[0].style;
+		var rv = {'backgroundColor': style.color || '#222221'};
+		if (style.logo) {
+		  rv.backgroundImage = 'url(' + style.logo + ')';
+		}
+		return rv;
+	},
     render: function() {
 		var topic = this.props.topic;
 		var user = this.props.topic.user;
-        var cafe = this.props.topic.cafes;
+        var cafe = this.props.topic.cafes[0];
         var webpage;
         if (topic.webpage) {
             webpage = <Webpage webpage={ topic.webpage } />;
         }
         return (
 			<div className="hentry">
+
 				<div className="container">
 					<h2 className="entry-title">{ topic.title }</h2>
 					<div className="entry-meta">
 						<a href={ "/c/" } aria-label={ "Published in " }>
-							<span className="cafe-logo" style={{"background-color":"#222221"}}></span>
+							<span className="cafe-logo" style={{"backgroundColor":"#222221"}}></span>
 						</a>
 						<time title={ "Updated at " + topic.updated_at }>{ timeago(topic.created_at) }</time>
 						<a href={ "/u/" + user.username } aria-label={ "Published by " + user.username }>@{ user.username }</a>
@@ -41,12 +56,12 @@ var TopicHentry = React.createClass({
 						<div className="topic-cafe column">
                             <div className="column-title">Published In</div>
 							<a href={ "/c/" } className="column-header">
-								<span className="cafe-logo"></span>
+								<span className="cafe-logo" style={ this.cafeStyle() }></span>
 								<div className="column-main">
-									<strong></strong>
+									<strong>{ cafe.name }</strong>
 								</div>
 							</a>
-							<p></p>
+							<p>{ urlize(cafe.description) }</p>
 						</div>
 
 						<div className="topic-author column">
