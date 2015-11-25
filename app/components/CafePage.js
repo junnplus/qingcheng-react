54742dd38e66ea5c9c3d2353bfabb988ac0fba92
junnplus <junnplus@qq.com>
2015-11-25 18:33:09 +0800

    cafe page add create button

--- app/components/CafePage.js
+++ app/components/CafePage.js
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 var Reflux = require('reflux');
 var Header = require('./Header');
 var CafeActions = require('../actions/CafeActions');
@@ -8,6 +10,8 @@ var FetchActions = require('../actions/FetchActions');
 var urlize = require('../filters').urlize;
 var TopicList = require('./TopicList');
 var TopicsStore = require('../stores/TopicsStore');
+var TopicsActions = require('../actions/TopicsActions');
+var UserAvatar = require('./UserAvatar');
 
 var CafePage = React.createClass({
     mixins: [
@@ -16,7 +20,9 @@ var CafePage = React.createClass({
     ],
     componentDidMount: function() {
         var slug = this.props.params.slug;
-		CafeActions.load(slug);
+		CafeActions.load(slug, function(){
+            TopicsActions.fetchCafeTopics(slug);
+        });
     },
     componentWillReceiveProps: function(nextProps) {
         var newSlug = nextProps.params.slug;
@@ -24,17 +30,36 @@ var CafePage = React.createClass({
         if ( oldSlug !== newSlug ) {
             this.setState({topics: []});
             FetchActions.fetching(true);
-            CafeActions.load(newSlug);
+            CafeActions.load(newSlug, function(){
+                TopicsActions.fetchCafeTopics(newSlug);
+            });
         }
+    },
+    canWrite: function() {
+        var permission = this.state.cafe.permission || {};
+        return permission.write;
     },
     render: function() {
         var cafe = this.state.cafe;
+		var current_user = this.props.current_user;
         return (
             <div className="cafe-view">
                 <Header title={cafe.name} description={urlize(cafe.description)} cafe={cafe} path="cafe" />
                 <div className="body">
 					<div className="split-view container">
 					  <div className="main-view">
+                        {
+                            (function(obj){
+                                if (obj.canWrite()) {
+                                    return (
+                                        <div className="new-topic">
+                                            <UserAvatar user={current_user} clazz="small circle"></UserAvatar>
+                                            <Link to={ "/c/" + cafe.slug + "/create" }>Create a new topic here</Link>
+                                        </div>
+                                    );
+                                }
+                            }(this))
+                        }
 						<TopicList slug={this.props.params.slug} topics={this.state.topics} />
 					  </div>
 					</div>
